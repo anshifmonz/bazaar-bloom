7ae8e1625243241adcad749e2c8c18e5c10481b8
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-09-16 22:28:05 +0530

    refactor(storeRoute): organized store routes into different files

--- server/routes/store/storeRoutes.js
+++ server/routes/store/storeRoutes.js
@@ -1,56 +1,15 @@
-import e from 'express';
+import express from 'express';
 
-import adminCheck from '../../middleware/adminCheckMiddleware.js';
-import { isAuthenticated } from '../../middleware/authCheckMiddleware.js';
+import productRouter from './productRoutes.js';
+import cartRouter from './cartRoutes.js';
+import orderRouter from './orderRoutes.js';
+import favRouter from './favRoutes.js';
 
-// product
-import showProduct from '../../controller/store/product/showProductController.js';
-import addProduct from '../../controller/store/product/addProductController.js';
-import updateProduct from '../../controller/store/product/updateProductController.js';
-import delProduct from '../../controller/store/product/delProductController.js';
+const router = express.Router();
 
-// cart
-import showCart from '../../controller/store/cart/showCartController.js';
-import addCartItem from '../../controller/store/cart/addCartItemController.js';
-import updateCartQuantity from '../../controller/store/cart/updateCartQuantityController.js';
-import delCartProduct from '../../controller/store/cart/delCartProductController.js';
-
-// favorite
-import showFavorite from '../../controller/store/favorites/showFavoriteController.js';
-import addFavProductController from '../../controller/store/favorites/addFavProduct.js';
-import rmFavProductController from '../../controller/store/favorites/rmFavProduct.js';
-
-// order
-import orderProduct from '../../controller/store/order/orderProductController.js';
-import orderCart from '../../controller/store/order/orderCartController.js';
-import orderCancel from '../../controller/store/order/orderCancelController.js';
-import showOrders from '../../controller/store/order/showOrdersController.js';
-import showAOrderDetails from '../../controller/store/order/showAOrderDetailController.js';
-
-const router = e.Router();
-
-// product
-router.get('/product', showProduct);
-router.post('/product', isAuthenticated, adminCheck, addProduct);
-router.put('/product', isAuthenticated, adminCheck, updateProduct);
-router.delete('/product', isAuthenticated, adminCheck, delProduct);
-
-// cart
-router.get('/cart', isAuthenticated, showCart);
-router.post('/cart', isAuthenticated, addCartItem);
-router.put('/cart', isAuthenticated, updateCartQuantity);
-router.delete('/cart', isAuthenticated, delCartProduct);
-
-// favorite
-router.get('/favorite', isAuthenticated, showFavorite);
-router.post('/favorite', isAuthenticated, addFavProductController);
-router.delete('/favorite', isAuthenticated, rmFavProductController);
-
-// order
-router.get('/orders/', isAuthenticated, showOrders);
-router.get('/orders/:orderId', isAuthenticated, showAOrderDetails);
-router.post('/order/product', isAuthenticated, orderProduct);
-router.post('/order/cart', isAuthenticated, orderCart);
-router.post('/order/cancel', isAuthenticated, orderCancel);
+router.use('/product', productRouter);
+router.use('/cart', cartRouter);
+router.use('/favorite', favRouter);
+router.use('/orders', orderRouter);
 
 export default router;
