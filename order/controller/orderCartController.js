bb41de47f8ceee2f40ad6fa5b7be5971155eeb06
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-16 09:50:01 +0530

    feat(order): improve response when insufficient stock

--- order/controller/orderCartController.js
+++ order/controller/orderCartController.js
@@ -6,7 +6,7 @@ const orderCartController = async (req, res) => {
   try {
     const resp = await orderCart(userId);
     if (resp === 'Cart is empty') return res.status(400).json({ success: false, message: resp });
-    if (resp?.noStock) return res.status(409).json({ success: false, data: resp });
+    if (resp?.noStock) return res.status(409).json({ success: false, data: resp.noStock, message: 'Not enough stock available' });
   
     res.status(201).json({ success: true });
   } catch (err) {
