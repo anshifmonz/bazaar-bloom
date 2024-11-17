6f776d1eafe71291df81534fd8007ca33bb5edee
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-17 11:01:36 +0530

    refactor(product): remove console log in productExistCheck route

--- product/controller/external/productExistCheckController.js
+++ product/controller/external/productExistCheckController.js
@@ -4,9 +4,7 @@ const productExistCheckController = async (req, res) => {
   const { productId } = req.params;
 
   try {
-    const resp = await isProductExist(productId);
-    console.log(resp);
-    
+    const resp = await isProductExist(productId);    
     if (resp) return res.status(404).json({ success: true, message: 'Product doesn\'t exist' });
 
     res.status(200).send('ok')
