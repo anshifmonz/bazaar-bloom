e4353550ac68890079091db1597f5b051ee8ecdf
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-28 21:14:49 +0530

    feat(user): add route to getCard details for external service

--- user/routes/cardRoutes.js
+++ user/routes/cardRoutes.js
@@ -4,6 +4,8 @@ import saveCardController from "../controller/card/saveCardController.js";
 import getCardController from "../controller/card/getCardController.js";
 import deleteCardController from "../controller/card/deleteCardController.js";
 
+import externalGetCardController from "../controller/card/externalGetCardController.js";
+
 import { isAuthenticated } from '../middleware/authCheckMiddleware.js';
 
 const router = e.Router();
@@ -12,7 +14,7 @@ router.post('/save-card', isAuthenticated, saveCardController);
 router.post('/get-card', isAuthenticated, getCardController);
 router.post('/delete-card', isAuthenticated, deleteCardController);
 
-router.get('/get-card', getCardController);
+router.get('/get-card', externalGetCardController);
 
 router.all('*', (req, res) => res.status(404).send('Invalid path'))
 
