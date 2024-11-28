5642900bbe8c4c9277b1c042b68db9fffa8dcc28
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-28 16:51:13 +0530

    feat(user): update user table

--- auth/db.sql
+++ auth/db.sql
@@ -1,10 +1,18 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL  CHECK (email <> ''),
-  password VARCHAR(255) NOT NULL CHECK (password <> ''),
-  role VARCHAR(10) DEFAULT 'user', -- admin or user
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(15),
+  address JSONB,
+  preferred_currency VARCHAR(10) DEFAULT 'USD',
+  language VARCHAR(20) DEFAULT 'en',
+  status VARCHAR(20) DEFAULT 'active', -- active, inactive, banned
+  is_verified BOOLEAN DEFAULT FALSE,
+  role VARCHAR(50) DEFAULT 'user', --user, admin, vendor
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "session" (
