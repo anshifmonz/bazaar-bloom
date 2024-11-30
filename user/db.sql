6214c487e23fc994d1d9213bcc62bc1215bd4535
Muhammed Anshif <muhammedanshif236@gmail.com>
2024-11-30 15:38:27 +0530

    feat(user): change address column from JSONB to JSONB[] in userDb

--- user/db.sql
+++ user/db.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   phone_number VARCHAR(15),
-  address JSONB,
+  address JSONB[],
   preferred_currency VARCHAR(10) DEFAULT 'USD',
   language VARCHAR(20) DEFAULT 'en',
   status VARCHAR(20) DEFAULT 'active', -- active, inactive, banned
