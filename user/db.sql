23485794d612a1a676e47a5318d955c18c4472a3
anshifmonz <muhammedanshif236@gmail.com>
2025-04-19 21:52:58 +0530

    fix(user): remove ON UPDATE clause from user_cards table and standardized JSON casing

--- user/db.sql
+++ user/db.sql
@@ -21,13 +21,13 @@ CREATE TABLE user_cards (
   card_token VARCHAR(255) NOT NULL,
   customer_id VARCHAR(255) NOT NULL,
   is_default BOOLEAN DEFAULT FALSE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Date the card was added
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Date the card record was last updated
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE "session" (
   "sid" VARCHAR(44) PRIMARY KEY NOT NULL,
-  "sess" json NOT NULL,
+  "sess" JSON NOT NULL,
   "expire" TIMESTAMP(3) NOT NULL
 );
